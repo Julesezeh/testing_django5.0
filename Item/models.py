7778cff18b216b3628f56cf28de2591a9396734a
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-12-04 21:32:03 +0100

    GeneratedField not available. 5.0 in development.

--- Item/models.py
+++ Item/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Fruit(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.FloatField()
+    number_available = models.IntegerField()
+    total_capital = models.GeneratedField()
